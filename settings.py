ec486dc33d569f915fc96c43edf49271b25b4e1f
Neil Muller <drnlmuller@gmail.com>
2019-03-21 09:46:23 +0200

    Open talk submissions

--- settings.py
+++ settings.py
@@ -105,8 +105,8 @@ MARKITUP_FILTER = ('markdown.markdown', {
 # Use HTTPS jquery URL so it's accessible on HTTPS pages (e.g. editing a talk)
 JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js'
 
-# Talks submissions aren't open yet
-WAFER_TALKS_OPEN = False
+# Talks submissions are open
+WAFER_TALKS_OPEN = True
 
 # Set the timezone to the conference timezone
 USE_TZ = True
